6f4aa6af771d31bde1aa0696df318370b2c9089d
PabloGoldman <pablo.goldmanarca@gmail.com>
2021-10-15 17:35:09 -0300

    Added SceneManager script.

--- SceneManager.h
+++ SceneManager.h
@@ -1,5 +1,19 @@
 #pragma once
+
+enum class Scene { MENU = 1, GAME, RULES, CREDITS, EXIT, CHOOSEMODE, ENDGAME };
+
+
 class SceneManager
 {
+private:
+	Scene scene;
+
+public:
+	SceneManager();
+	~SceneManager();
+
+	Scene GetScene();
+
+	void SetSceneManager(Scene _scene);
 };
 
